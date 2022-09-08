1be42bdce446321cc3a1b659933dcca79a58426b
Ruy Victor <ruy.victor@clicksoft.com.br>
2022-09-08 12:40:00 -0300

    fix: undefined array in location destructuring

--- dist/helpers/create-about.js
+++ dist/helpers/create-about.js
@@ -10,21 +10,21 @@ class About {
         this.document = new jsdom_1.default.JSDOM(html).window.document;
     }
     create() {
-        var _a, _b, _c, _d;
+        var _a, _b, _c, _d, _e;
         const name = this.document.querySelector(".top-card-layout__title");
         const title = this.document.querySelector(".top-card-layout__headline");
         const description = this.document.querySelector(".core-section-container__content");
         const location = this.document.querySelector(".top-card__subline-item:nth-child(1)");
-        const [city, state, country] = (_a = location === null || location === void 0 ? void 0 : location.textContent) === null || _a === void 0 ? void 0 : _a.trim().split(", ");
+        const [city, state, country] = (_b = (_a = location === null || location === void 0 ? void 0 : location.textContent) === null || _a === void 0 ? void 0 : _a.trim().split(", ")) !== null && _b !== void 0 ? _b : [];
         this.about = {
-            name: (_b = name.textContent) === null || _b === void 0 ? void 0 : _b.trim(),
-            title: (_c = title.textContent) === null || _c === void 0 ? void 0 : _c.trim(),
+            name: (_c = name.textContent) === null || _c === void 0 ? void 0 : _c.trim(),
+            title: (_d = title.textContent) === null || _d === void 0 ? void 0 : _d.trim(),
             location: {
                 city,
                 state,
                 country,
             },
-            description: (_d = description === null || description === void 0 ? void 0 : description.textContent) === null || _d === void 0 ? void 0 : _d.trim(),
+            description: (_e = description === null || description === void 0 ? void 0 : description.textContent) === null || _e === void 0 ? void 0 : _e.trim(),
         };
         return this.about;
     }
